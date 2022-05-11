d58a0a3d65f7d8e730fe15e217bbd5648fb352ee
Marta Johnson <martajohnson@Martas-MacBook-Pro.local>
2022-05-11 12:12:31 -0500

    Basic functionality except for getting the real contract from backend

--- src/index.js
+++ src/index.js
@@ -1,19 +1,113 @@
+// const ethers = require('ethers');
+
+// const provider = new ethers.providers.Web3Provider(wallet);
+
+
+// const erc721AbbreviatedAbi = [
+//   {
+//     inputs: [
+//       {
+//         internalType: 'address',
+//         name: 'owner',
+//         type: 'address',
+//       },
+//     ],
+//     name: 'balanceOf',
+//     outputs: [
+//       {
+//         internalType: 'uint256',
+//         name: '',
+//         type: 'uint256',
+//       },
+//     ],
+//     stateMutability: 'view',
+//     type: 'function',
+//   },
+// ];
+
 wallet.registerRpcMessageHandler(async (originString, requestObject) => {
   switch (requestObject.method) {
     case 'hello':
-      return wallet.request({
-        method: 'snap_confirm',
-        params: [
-          {
-            prompt: `Hello, ${originString}!`,
-            description:
-              'This custom confirmation is just for display purposes.',
-            textAreaContent:
-              'But you can edit the snap source code to make it do something, if you want to!',
-          },
-        ],
-      });
+      // const got = await provider.listAccounts();
+      // console.log({ got });
+      const contract = await getContract(originString);
+
+      if (!contract) {
+        await showUserMessage(
+          'Could not verify site project',
+          `We couldn't verify that this site is associated with a SuppDapp project. Please proceed with caution, as it may be a scam.`,
+        );
+        return {
+          valid: false,
+        }
+      }
+
+      const etherscanAddress = getEtherscanAddress(contract);
+
+      // const contractInstance = new ethers.Contract(contract, erc721AbbreviatedAbi, provider);
+
+      let openseaUrl;
+      let error;
+      try {
+        openseaUrl = await getOpenseaUrl(contract);
+      } catch (err) {
+        error = err;
+      }
+
+      if (error || !openseaUrl) {
+        showUserMessage(
+          `Verified project`,
+          `We verified that this url is associated with the contract at ${etherscanAddress} but were not able to find an associated OpenSea collection. Carefully review this information; if it does not match your expectations, it may be a scam.`
+        )
+        return {
+          valid: true,
+          contract,
+        }
+      }
+
+      showUserMessage(
+        `Verified project`,
+        `We verified that this url is associated with the contract at ${etherscanAddress} and at OpenSea at ${openseaUrl}. Carefully review this information; if it does not match your expectations, it may be a scam.`
+      );
+
+      return { valid: true, contract };
     default:
       throw new Error('Method not found.');
   }
 });
+
+async function getContract(url) {
+  return '0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d'; // TODO
+}
+
+function getEtherscanAddress(contract) {
+  return `https://etherscan.io/address/${contract}`;
+}
+
+function getCollectionUrl(slug) {
+  return `https://opensea.io/collection/${slug}`;
+}
+
+async function getOpenseaUrl(contract) {
+  const response = await fetch(
+    `https://api.opensea.io/api/v1/asset_contract/${contract}`,
+  );
+  if (!response.ok) {
+    return null;
+  }
+  const result = await response.json();
+  const { slug } = result.collection;
+  return getCollectionUrl(slug);
+}
+
+function showUserMessage(title, content) {
+  return wallet.request({
+    method: 'snap_confirm',
+    params: [
+      {
+        prompt: title,
+        textAreaContent: content,
+      },
+    ],
+  });
+}
